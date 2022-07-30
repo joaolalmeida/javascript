267c2a3415d87cd73968e8d4d7276a934c17a8d2
João Almeida <108037819+joaolalmeida@users.noreply.github.com>
2022-07-30 18:23:28 -0300

    update

--- exercicios/ex002 - Tratamento de dados/js/script.js
+++ exercicios/ex002 - Tratamento de dados/js/script.js
@@ -1,2 +1,64 @@
 var nome = window.prompt("Qual é o seu nome?"); //vai perguntar o nome ...
 alert("É um grande prazer em te conhecer, " + nome + "!"); // sinal de + é uma concatenação ...
+document.write(
+  `<p class="teste">Olá ${nome}! Seu nome tem ${nome.length} letras</p>`
+); //   write é escreva no caso (escreva no documento)
+document.write(
+  `<p class="teste">Seu nome em maiúsculo é ${nome.toUpperCase()}</p>`
+); // escrever tudo em MAIÚSCULO
+document.write(
+  `<p class="teste">Seu nome em minúsculas é ${nome.toLowerCase()}</p>`
+);
+var n1 = Number(window.prompt("qual é o seu número favorito?"));
+var n2 = Number(window.prompt(`agora fale um outro número!`));
+var n3 = n1 / n2;
+var n4 = n2 / n1;
+
+document.write(`Operações com o número ${n1}! <br/>`);
+document.write(`<br/>`);
+
+document.write(`Seu número favorito somado a ele mesmo é ${n1 + n1}! <br/>`);
+document.write(`Seu número favorito vezes ele mesmo é ${n1 * n1}! <br/>`);
+document.write(`Seu número favorito elevado a ele mesmo é ${n1 ** n1}! <br/>`);
+
+document.write(`<br/>`);
+
+document.write(`Operações com o número ${n2}! <br/>`);
+document.write(`<br/>`);
+
+document.write(`Seu outro número somado a ele mesmo é ${n2 + n2}! <br/>`);
+document.write(`Seu outro número vezes ele mesmo é ${n2 * n2}! <br/>`);
+document.write(`Seu outro número elevado a ele mesmo é ${n2 ** n2}! <br/>`);
+
+document.write(`<br/>`);
+
+document.write(`Operações com os números ${n1} e ${n2}! <br/>`);
+document.write(`<br/>`);
+
+document.write(
+  `Seu número favorito  mais o outro número que você digitou antes é ${
+    n1 + n2
+  } <br/>`
+);
+document.write(`Seu número favortio menos o outro número é ${n1 - n2} <br/>`);
+document.write(
+  `Seu outro número menos o seu número favorito é ${n2 - n1} <br/>`
+);
+document.write(`Seu número favorito vezes o outro número é ${n1 * n2} <br/>`);
+document.write(
+  `Seu outro número vezes o seu número favorito é ${n2 * n1} <br/>`
+);
+document.write(
+  `Seu número favorito dividido pelo seu outro número é ${n3.toFixed(2)} <br/>`
+);
+document.write(
+  `Seu número outro número dividido pelo seu número favorito é ${n4.toFixed(
+    2
+  )} <br/>`
+);
+document.write(
+  `Seu número favorito elevado ao seu outro número é ${n1 ** n2} <br/>`
+);
+document.write(
+  `Seu outro número elevado ao seu número favorito é ${n2 ** n1} <br/>`
+);
--- exercicios/ex002 - Tratamento de dados/js/script2.js
+++ exercicios/ex002 - Tratamento de dados/js/script2.js
@@ -1,6 +1,15 @@
-var n1 = window.prompt("Digite um número: "); // string
-var n2 = window.prompt("Digite outro número: "); // string
+var n1 = Number.parseInt(window.prompt("Digite um número: ")); // string
+var n2 = Number.parseInt(window.prompt("Digite outro número: ")); // string
 var s = n1 + n2;
 window.alert("A soma dos valores é " + s); // concatenação
 // (number + numer) para adição
 // (string + string) para concatenação
+
+// Modo simplificado que o js decide qual dado está sendo inserido
+
+var num1 = Number(prompt("Digite aqui um numero! "));
+var num2 = Number(prompt("Digite aqui outro número! "));
+var som = num1 + num2;
+alert("A soma dos valores é " + som);
+alert("A soma dos valores é " + String(som)); // transformando um number em uma string
+alert(`A soma entre ${num1} e ${num2} é igual a ${som}`); // precisa utilizar crase `` para este modo
