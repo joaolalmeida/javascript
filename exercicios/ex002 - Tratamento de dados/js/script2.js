0e752ae2ff75889b63e6e9f037298e188c2dc0d1
João Almeida <108037819+joaolalmeida@users.noreply.github.com>
2022-07-30 14:36:47 -0300

    update js

---
+++ exercicios/ex002 - Tratamento de dados/js/script2.js
@@ -0,0 +1,6 @@
+var n1 = window.prompt("Digite um número: "); // string
+var n2 = window.prompt("Digite outro número: "); // string
+var s = n1 + n2;
+window.alert("A soma dos valores é " + s); // concatenação
+// (number + numer) para adição
+// (string + string) para concatenação
