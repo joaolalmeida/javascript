f122002d9b4d396f6da130f09e2d03e0ee68ad00
João Almeida <108037819+joaolalmeida@users.noreply.github.com>
2022-08-01 18:53:24 -0300

    update

---
+++ exercicios/ex003 - Introdução ao DOM/js/script.js
@@ -0,0 +1,6 @@
+    var corpo = document.body;
+    var p1 = document.getElementsByTagName("p")[1];
+    var d = document.getElementById('msg')  
+    document.write(p1.innerText);
+    d.style.background = 'green'
+    document.write(msg.innerHTML);
