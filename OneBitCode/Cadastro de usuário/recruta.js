942c9bfc319bddd0daa6df3fcb479632a79701e3
João Almeida <108037819+joaolalmeida@users.noreply.github.com>
2022-09-08 16:23:20 -0300

    update

---
+++ OneBitCode/Cadastro de usuário/recruta.js
@@ -0,0 +1,5 @@
+let nome = window.prompt("Digite seu Nome Completo")
+let estudo = prompt("Digite seu campo de estudo")
+let nascimento = prompt("Digite seu ano de nascimento")
+
+alert(`informações de cadastro:\n Nome Completo: ${nome} \n Area de Estudo: ${estudo} \n Idade: ${2022 - nascimento} `)
---
+++ OneBitCode/Calculadora de 4 operações/calculadora.js
@@ -0,0 +1,11 @@
+const entrada1 = prompt("Digite um número: ")
+const entrada2 = prompt("Digite outro número: ")
+
+const n1 = parseFloat(entrada1)
+const n2 = parseFloat(entrada2)
+alert(`                   -------Informações--------\n
+ ----- Os valores ${n1} e ${n2} nas seguintes Operações---- \n
+ Soma:    ${n1 + n2}  \n
+ Subtração:    ${n1 - n2}  \n
+ Multiplicação:   ${n1 * n2}  \n
+ Divisão:   ${n1 / n2}`)
