5dad336c4c77403facbb65f209f8fe92b6f6694b
João Almeida <108037819+joaolalmeida@users.noreply.github.com>
2022-10-31 14:40:12 -0300

    update elements Dom

--- OneBitCode/DOM/Obtendo elementos HTML do DOM/index.js
+++ OneBitCode/DOM/Obtendo elementos HTML do DOM/index.js
@@ -9,6 +9,23 @@ function show() {
   const listElements = document.getElementsByTagName("li")
   console.log(listElements)
 
+  // Obter vários elementos (HTMLCollection) a partir de uma classe
+  const contactInputs = document.getElementsByClassName("contact-input")
+  console.log(contactInputs)
 
+  //  // Obter vários elementos (NodeList) a partir do atributo name
+  const contact1 = document.getElementsByName("contact1")
+  console.log(contact1)
+
+  // E o método mais flexível de todos, através de uma query (semelhante ao que fazemos com o CSS):
+  const contacts = document.querySelectorAll("#contact-list > li > label ")
+  console.log(contacts)
+
+  // Além disso, também é possível obter um único elemento através de uma query. O comportamento é igual ao do querySelectorAll(), porém selecionando o primeiro elemento que corresponder à query:
+  // Obs.: Repare que também podemos acessar propriedade internas do elemento obtido.
+  
+  const contact = document.querySelector("#contact-list > li > label")
+  console.log(contact)
+  console.log(contact.textContent)
 }
 
