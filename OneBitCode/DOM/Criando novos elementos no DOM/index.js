e15682339113291bb5bed4589233a2261ded76d9
João Almeida <108037819+joaolalmeida@users.noreply.github.com>
2022-11-02 09:24:31 -0300

    cor do input

--- OneBitCode/DOM/Criando novos elementos no DOM/index.js
+++ OneBitCode/DOM/Criando novos elementos no DOM/index.js
@@ -1,5 +1,5 @@
 function addInput() {
-  const ul = document.getElementById('inputs')
+  const ul = document.querySelector('#inputs')
 
   const newLi = document.createElement('li') // Tag a ser criada
   newLi.className = 'list-item'  // nome da classe do li criado
@@ -10,6 +10,10 @@ function addInput() {
   newInput.name = 'input'
   newInput.id = 'newInputCreated' // criando uma id para o input
   newInput.placeholder = 'Escreva aqui' // Texto dentro do input
+  newInput.style.width = "300px" // largura do input
+  newInput.style.margin = "10px" // margin entre os inputs
+  newInput.style.background = "lightgreen" // cor do input
+  
 
   newLi.appendChild(newInput) //newLi é o elemento pai recebendo o newInput
   ul.appendChild(newLi)  // ul é o elemento pai 
