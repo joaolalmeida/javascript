da1dedbb9bc61ebbf4b732e296508a78a32cb09a
João Almeida <108037819+joaolalmeida@users.noreply.github.com>
2022-11-02 20:51:26 -0300

    criando função adicionar jogador

---
+++ OneBitCode/DOM/Escalação de Times_Exercício/index.js
@@ -0,0 +1,15 @@
+function addPlayer() {
+  const position = document.getElementById('position').value
+  const name = document.getElementById('name').value
+  const number = document.getElementById('number').value
+
+  const confirmation = confirm("Escalar " + name + " como " + position + "?")
+
+  if (confirmation) {
+    const teamList = document.getElementById('teamList')
+    const playerItem = document.createElement('li')
+    playerItem.id = 'player-' + number
+    playerItem.innerText = position + ": " + name + "(" + number + ")"
+    teamList.appendChild(playerItem)
+  }
+}
