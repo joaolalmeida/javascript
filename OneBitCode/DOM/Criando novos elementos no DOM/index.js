b22ff674af28b6de33109cbffdc3ad678202fb97
João Almeida <108037819+joaolalmeida@users.noreply.github.com>
2022-11-02 09:18:45 -0300

    anotações no js

--- OneBitCode/DOM/Criando novos elementos no DOM/index.js
+++ OneBitCode/DOM/Criando novos elementos no DOM/index.js
@@ -2,12 +2,14 @@ function addInput() {
   const ul = document.getElementById('inputs')
 
   const newLi = document.createElement('li') // Tag a ser criada
-  newLi.className = 'list-item'
-  newLi.innerText = 'Novo input: '
+  newLi.className = 'list-item'  // nome da classe do li criado
+  newLi.innerText = 'Novo input: '  // Nome do texto que sera escrito no li
 
   const newInput = document.createElement('input')
   newInput.type = 'text'  // Type e name os tipos do input
   newInput.name = 'input'
+  newInput.id = 'newInputCreated' // criando uma id para o input
+  newInput.placeholder = 'Escreva aqui' // Texto dentro do input
 
   newLi.appendChild(newInput) //newLi é o elemento pai recebendo o newInput
   ul.appendChild(newLi)  // ul é o elemento pai 
