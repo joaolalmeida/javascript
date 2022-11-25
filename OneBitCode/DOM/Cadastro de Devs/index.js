9256ffdd5c67395cdd15339557294b5284959d94
João Almeida <108037819+joaolalmeida@users.noreply.github.com>
2022-11-25 17:43:23 -0300

    javascript Finalizado

--- OneBitCode/DOM/Cadastro de Devs/index.js
+++ OneBitCode/DOM/Cadastro de Devs/index.js
@@ -32,8 +32,22 @@ addTechBtn.addEventListener('click', function (ev) {
   const techNameLabel = createLabel('Nome: ', 'techName-' + rowIndex)
   const techNameInput = createInput('techName-' + rowIndex, null, 'techName')
 
+  const expLabel = createLabel('Experiência: ')
+  const id1 = 'expRadio-' + rowIndex + '.1'
+  const expRadio1 = createInput(id1, '0-2 anos', 'techExp-' + rowIndex, 'radio') // a **virgula** separa o Id do value do name etc e o + é para adicionar mais coisas no mesmo local
+  const expLabel1 = createLabel('0-2 anos', id1)
+  
+  const id2 = "expRadio-" + rowIndex + ".2"
+  const expRadio2 = createInput(id2, "3-4 anos", "techExp-" + rowIndex, "radio") 
+  const expLabel2 = createLabel("3-4 anos", id2)
+
+  const id3 = "expRadio-" + rowIndex + ".3"
+  const expRadio3 = createInput(id3, "5+ anos", "techExp-" + rowIndex, "radio") 
+  const expLabel3 = createLabel("5+ anos", id3)
+
+
   newRow.append(
-    techNameLabel, techNameInput
+    techNameLabel, techNameInput, expLabel, expRadio1, expLabel1, expRadio2, expLabel2, expRadio3, expLabel3
   )
   stackInputs.appendChild(newRow)
 })
