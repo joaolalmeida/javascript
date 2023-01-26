d197c3b364a4b364c99c15567d0851a5817035d0
João Almeida <108037819+joaolalmeida@users.noreply.github.com>
2023-01-26 19:46:00 -0300

    teste

---
+++ OneBitCode/DOM/Jodo da Velha/index.js
@@ -0,0 +1,19 @@
+const boardRegions = document.querySelectorAll("#gameBoard span")
+const vBoard = []
+let turnPlayer = "dd"
+
+function updateTitle() {
+  const playerInput = document.getElementById(turnPlayer)
+  document.getElementById("turnPlayer").innerText = playerInput.value
+}
+
+function initializeGame() {
+  vBoard = [["", "", ""], ["", "", ""][("", "", "")]]
+  turnPlayer = 'player1'
+  document.querySelector('h2').innerHTML = 'Vez de: <span id="turnPlayer"></span>'
+  updateTitle()
+  boardRegions.forEach(function (element) {
+    element.classList.remove('win')
+  })
+}
+
