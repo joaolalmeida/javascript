8f8f5015b04a59cfa24aa6f2f7baf4c6208d7c19
João Almeida <108037819+joaolalmeida@users.noreply.github.com>
2023-02-15 09:34:37 -0300

    finalizando exercicio

--- OneBitCode/JS Moderno/Rest params/index.js
+++ OneBitCode/JS Moderno/Rest params/index.js
@@ -2,4 +2,4 @@ function sum(...numbers) {
   return numbers.reduce((accum, num) => accum + num, 0) /*somar numeros dentro de um array */
 }
 
-console.log(sum(1,5,2,6))
+console.log(sum(32,5,74,7,1,9,90))
