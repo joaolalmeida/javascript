c611813af00e16cd5b380e849195c77797d331c8
João Almeida <108037819+joaolalmeida@users.noreply.github.com>
2023-02-16 11:36:49 -0300

    exercicio finalizado

--- OneBitCode/JS-Moderno/Tipos_de_export_do_ESM/index.js
+++ OneBitCode/JS-Moderno/Tipos_de_export_do_ESM/index.js
@@ -1,4 +1,7 @@
-
+import { inline } from "./inline.js"
+import defaultInline from "./inline.js"
+import { group } from "./non-inline.js"
+import exportDefault from "./non-inline.js"
 
 inline()
 defaultInline()
