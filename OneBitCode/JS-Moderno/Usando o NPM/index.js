233fa0536bd46a66e17266d0e50fcbfef9663ee5
João Almeida <108037819+joaolalmeida@users.noreply.github.com>
2023-02-20 15:28:45 -0300

    novo exercicio

---
+++ OneBitCode/JS-Moderno/Usando o NPM/index.js
@@ -0,0 +1,4 @@
+const lodash = require("lodash") /* pegando o caminho para importar o modulo instalado */
+
+console.log(lodash.isArray([]))
+console.log(lodash.kebabCase("João vitor lopes de almeida"))
